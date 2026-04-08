6fa32d0f8bec9dffcb2ad007e2725dca1028f58d
testphuoc <vuducphuocvn@gmail.com>
2026-04-08 18:23:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 
 app.use(express.json());
 
+// test server
 app.get("/", (req, res) => {
   res.send("HRM OK 🚀");
 });
 
+// API check-in
 app.post("/checkin", (req, res) => {
-  res.send("Check-in OK");
+  const { user_id, lat, lng } = req.body;
+
+  res.json({
+    message: "Check-in OK",
+    data: { user_id, lat, lng }
+  });
 });
 
-app.listen(3000);
+// PORT cho Railway
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server chạy " + PORT);
+});
